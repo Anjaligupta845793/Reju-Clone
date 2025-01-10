a47afe5980f974ac616d539f6ea6f086b6bfaf5e
anjali gupta <sajalanjali3@gmail.com>
2025-01-10 17:38:13 +0530

    add

--- model/Module.js
+++ model/Module.js
@@ -1,22 +1,111 @@
 import mongoose from "mongoose";
 
-const Module = new mongoose.Schema({
-  type: String,
+const ModuleSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: [
+      "External Link",
+      "Media Gallery",
+      "Music",
+      "Youtube",
+      "Tiktok",
+      "Custom Product",
+      "Throne Wishlist",
+      "Custom Event",
+      "BandsInTown",
+      "Data Capture Form",
+    ],
+  },
   name: String,
   FormType: String,
   visible: bool,
+
   items: [{ type: [mongoose.Schema.Types.Mixed], default: [] }],
 });
 
-const MusicModule = new mongoose.Schema({});
+export const Module =
+  mongoose.models.Module || mongoose.model("Module", ModuleSchema);
 
-const MediaGallaryModule = new mongoose.Schema({});
+const MediaGallaryModule = new mongoose.Schema({
+  image: {
+    url: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  Url: {
+    type: String,
+    required: true,
+  },
+});
+export const MediaGallary =
+  mongoose.models.MediaGallary ||
+  mongoose.model("MediaGallary", MediaGallaryModule);
 
-const YouTubeTiktokModule = new mongoose.Schema({});
+const YouTubeTiktokModule = new mongoose.Schema({
+  link: {
+    type: String,
+    required: true,
+  },
+});
+export const YouTubeTiktok =
+  mongoose.models.YouTubeTiktok ||
+  mongoose.model("YouTubeTiktok", YouTubeTiktokModule);
 
-const ExternalLinkModule = new mongoose.Schema({});
+const ExternalLinkModule = new mongoose.Schema({
+  image: {
+    url: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  Url: {
+    type: String,
+    required: true,
+  },
+  Title: {
+    type: String,
+    required: true,
+  },
+});
 
-const EventModule = new mongoose.Schema({});
+export const ExternalLink =
+  mongoose.models.ExternalLink ||
+  mongoose.model("ExternalLink", ExternalLinkModule);
+
+const EventModule = new mongoose.Schema({
+  EventDate: {
+    type: String,
+    required: true,
+  },
+  VenueName: {
+    type: String,
+    required: true,
+  },
+  Location: {
+    type: String,
+    required: true,
+  },
+  TicketLink: {
+    type: String,
+    required: true,
+  },
+});
+export const Event =
+  mongoose.models.Event || mongoose.model("Event", EventModule);
 
 const ProductModule = new mongoose.Schema({
   image: {
@@ -46,3 +135,41 @@ const ProductModule = new mongoose.Schema({
     required: true,
   },
 });
+
+export const Product =
+  mongoose.models.Product || mongoose.model("Product", ProductModule);
+
+const FormModule = new mongoose.Schema({
+  image: {
+    url: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  Subtitle: {
+    type: String,
+    required: true,
+  },
+});
+export const Form = mongoose.models.Form || mongoose.model("Form", FormModule);
+
+const ThroneWishlistOrBandInTownModule = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+});
+export const ThroneWishlistOrBandInTown =
+  mongoose.models.ThroneWishlistOrBandInTown ||
+  mongoose.model(
+    "ThroneWishlistOrBandInTown",
+    ThroneWishlistOrBandInTownModule
+  );
